c653e10b7adc5655139e6a3add6b59298606d0f8
Özkan Bırak <133165922+OzkanBirak@users.noreply.github.com>
2024-05-17 00:59:46 +0300

    Create main.cpp

---
+++ First/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Student.h"
+#include "Course.h"
+#include "Registration.h"
+using namespace std;
+
+int main(){  
+//  Student s1 = Student(0, "", 0, "", "", "", 0);
+  //s1.getInfo();
+
+  return 0;
+}
