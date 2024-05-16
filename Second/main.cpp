ffa182e7d442dfe744118dc373d8258775887277
Özkan Bırak <133165922+OzkanBirak@users.noreply.github.com>
2024-05-17 01:35:41 +0300

    Update main.cpp

--- Second/main.cpp
+++ Second/main.cpp
@@ -11,10 +11,53 @@ int main() {
     Course course1(101, "Introduction to Programming", 3, "Dr. Smith", CourseType::LECTURE);
     Course course2(102, "Data Structures", 4, "Dr. Johnson", CourseType::LAB);
 
+
+    //bizim dersler
+    Course course3 (103, "General Physics I", 4, "Dr. M. Fatih Hasoglu", CourseType::LECTURE);
+    Course course4 (103, "General Physics II", 4, "Dr. M. Fatih Hasoglu", CourseType::LECTURE);
+    Course course5 (105, "Technical English", 3, "Prof. Robert Harris", CourseType::LECTURE);
+    Course course6 (106, "Calculus", 4, "Dr. Laura Chen", CourseType::LECTURE);
+    Course course7 (107, "Introduction to Computer Science", 3, "Dr. John", CourseType::LECTURE);
+    Course course8 (108, "Introduction to Computer Science Lab", 2, "Dr. John", CourseType::LAB);
+    Course course9 (109, "Introduction to Software Programming", 3, "Dr. Olivia Martinez", CourseType::LECTURE);
+    Course course10 (110, "Introduction to Software Programming Lab", 3, "Dr. Olivia Martinez", CourseType::LAB);
+    Course course11 (111, "Database Systems", 3, "Dr. Emma Wilson", CourseType::LECTURE);
+    Course course12 (112, "Database Systems Lab", 3, "Dr. Emma Wilson", CourseType::LAB);
+    Course course13 (113, "Introduction to Operating Systems", 2, "Dr. Michael Davis", CourseType::LECTURE);
+    Course course15 (114, "Artificial Intelligence", 4, "Prof. Christopher Moore", CourseType::LECTURE);
+    Course course16 (115, "Artificial Intelligence Lab", 4, "Prof. Christopher Moore", CourseType::LAB);
+    Course course17 (116, "Machine Learning", 4, "Dr. Sophia Anderson", CourseType::LECTURE);
+    Course course18 (117, "Machine Learning Lab", 4, "Dr. Sophia Anderson", CourseType::LAB);
+    Course course19 (118, "Natural Language Processing", 3, "Prof. Emily Thompson", CourseType::LECTURE);    
+    Course course20 (120, "Full-Stack Web Development" , 4, "Prof. Jane Doe", CourseType::LECTURE);
+    
+
     // Display available courses
     cout << "Available Courses:" << endl;
     course1.displayCourseInfo();
     course2.displayCourseInfo();
+
+    //sonradan eklenen dersler
+    course3.displayCourseInfo();
+    course4.displayCourseInfo();
+    course5.displayCourseInfo();
+    course6.displayCourseInfo();
+    course7.displayCourseInfo();
+    course8.displayCourseInfo();
+    course9.displayCourseInfo();
+    course10.displayCourseInfo();
+    course11.displayCourseInfo();
+    course12.displayCourseInfo();
+    course13.displayCourseInfo();
+    course14.displayCourseInfo();
+    course15.displayCourseInfo();
+    course16.displayCourseInfo();
+    course17.displayCourseInfo();
+    course18.displayCourseInfo();
+    course19.displayCourseInfo();
+    course20.displayCourseInfo();
+
+    
     cout << endl;
 
     // Create a student
